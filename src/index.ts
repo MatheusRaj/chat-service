0ac6680ff9ff4126be478a5e496fa034c13e5bd3
MatheusRaj <matheusraj5@gmail.com>
2022-03-10 14:43:40 -0300

    create return typ function

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import { config, listenWebsocket, Socket } from "mercurius-chat";
 import { listenRabbitTopic, publishRabbitMessage } from "mercurius-chat/dist/rabbit";
-import { persistMessage, listMessages } from "mercurius-chat/dist/mongo";
+import { persistMessage, listMessages, model, Schema } from "mercurius-chat/dist/mongo";
 
 interface IConversation {
   room: string;
@@ -15,38 +15,43 @@ const initApp = async () => {
     port: 3001,
     sentryKey: "",
     rabbitParams: {
-      dsn: "<YOUR-RABBIT-CONNECTION-HERE>",
-      exchange: "test",
+      dsn: "amqp://aquzikoa:8fKSvFoTLXwXj93KHNGBwn4YDoMEY1z9@small-ant.rmq.cloudamqp.com:5672/jobzz",
+      exchange: "eduzz",
       exchangeType: "topic",
-      connectionName: "test",
+      connectionName: "jobzz.chat.service",
     },
     mongoParams: {
-      mongoDatabase: "test",
-      mongoUrl: "<YOUR-MONGO-CONNECTION-HERE>"
-    },
-    redisUrl: "<YOUR-REDIS-CONNECTION-HERE>"
+      mongoDatabase: "jobzz-chat",
+      mongoUrl: "mongodb+srv://matheus:0388Dac4.@cluster0.yhcjh.mongodb.net/jobzz-chat"
+    }
   });
 
+  const ConversationModel = model('Conversation', new Schema({ room: String, message: { from: String, content: String } }));
+
   listenRabbitTopic({
-      queue: "test",
-      topic: "test",
-    }, (payload: IConversation) => {
-      persistMessage({ ...payload });
+      queue: "jobzz.chat.service",
+      topic: "jobzz.chat.service",
+    }, async (payload: IConversation) => {
+      console.log('PERSISTING THIS: ', payload);
+      persistMessage({ ...payload }, ConversationModel);
+
+      return true;
     }
   );
 
   listenWebsocket('join', async (socket: Socket, payload: IConversation) => {
     socket.join(payload.room);
 
-    const messages = await listMessages(payload);
+    const messages = await listMessages(payload, ConversationModel);
 
     socket.emit('list-messages', messages);
   });
 
   listenWebsocket('send', (socket: Socket, payload: IConversation) => {
+    console.log('EMITTING THIS: ', payload);
     socket.to(payload.room).emit('receive', payload);
 
-    publishRabbitMessage('test', payload);
+    publishRabbitMessage('jobzz.chat.service', payload);
   });
 };
 
